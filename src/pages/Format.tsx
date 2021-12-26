a0f641c57a45acb3fc28912046d8ca0286e923e6
Amine tchita <amine.tchita1@gmail.com>
2021-12-26 13:50:23 +0100

    format page

--- src/pages/Format.tsx
+++ src/pages/Format.tsx
@@ -1,7 +1,72 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Quiz } from "../utils/colors";
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: flex-start;
+
+  flex-direction: column;
+  margin-top: 2rem;
+  width: 100%;
+
+  textarea {
+    width: 100%;
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    padding: 1rem;
+    border-radius: 8px;
+    border: 2px solid #333;
+    height: calc(100vh - 2rem - 124px - 2rem - 19px - 3rem - 48px - 2rem);
+  }
+
+  button {
+    margin-bottom: 2rem;
+  }
+`;
+const vQuiz: Quiz = {} as Quiz;
 export default function Format() {
+  const [code, setcode] = useState("");
+
+  const handleChange = (val) => {
+    setcode(val.target.value);
+  };
+
+  const handleClick = () => {
+    // console.log(code);
+    const listQstSeg = code.split("QST-");
+    // for (let index = 0; index < listQstSeg.length; index++) {
+    //   const qstFrame = listQstSeg[index];
+
+    //   const tmpDoc = document.createElement("div");
+    //   tmpDoc.innerHTML = qstFrame;
+
+    //   const list = tmpDoc?.querySelectorAll("li p");
+    //   if (list) {
+    //     vQuiz._id= index + 1
+    //     vQuiz.questions=[]
+    //     for (let index = 0; index < list.length; index++) {
+    //       const element = list[index];
+    //       debugger;
+    //       if (element?.querySelector("span")?.style.color == "rgb(112, 173, 71)") {
+    //         // obj._id = index + 1;
+    //         console.log(element);
+    //       }else{
+    //         vQuiz.questions.push({lib})
+    //       }
+    //     }
+    //     listElements.push(obj);
+    //   }
+    // }
+  };
+
   return (
-    <div>
-      <p>format</p>
-    </div>
+    <StyledDiv>
+      <label htmlFor="code">Code:</label>
+      <textarea onChange={(val) => handleChange(val)} placeholder="Paste </> here" value={code} name="code" id="code" cols={30} rows={10}></textarea>
+      <button onClick={() => handleClick()} className="btn filled">
+        Convert to json
+      </button>
+    </StyledDiv>
   );
 }
--- src/utils/colors.tsx
+++ src/utils/colors.tsx
@@ -2,3 +2,23 @@ export const COLORS = {
   primary: "#0071BA",
   secondary: "#767676"
 };
+
+export interface Quiz {
+  _id: number;
+  type: string;
+  title: string;
+  incipit: string;
+  questions: Question[];
+  excipit: string;
+}
+
+export interface Question {
+  lib: string;
+  items: Feedback[];
+  feedbacks: Feedback[];
+}
+
+export interface Feedback {
+  label: string;
+  value: string;
+}
