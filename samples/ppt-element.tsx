ed41406c9e141517dde300d226ed33f8040e8a0d
Amine tchita <amine.tchita1@gmail.com>
2021-12-29 02:07:19 +0100

    add samples for formatting

---
+++ samples/ppt-element.tsx
@@ -0,0 +1,179 @@
+export const elm = `
+<p id="isPasted"
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Compl&eacute;tez
+        : Historiquement, le travail s&#39;effectue...</span></span>
+</p>
+
+<ol
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; clear: left; font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial; color: rgb(0, 0, 0); font-size: 12px; margin: 0px;'>
+<li
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; font-size: 10.72px; margin: 0px 0px 0px 20.1px; font-style: inherit; font-weight: inherit;">
+
+    <p
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; font-style: inherit; font-weight: inherit;">
+        <span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(112, 173, 71); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Dans
+            un m&ecirc;me espace-temps, dans un cadre organisationnel stable, et dans une proximit&eacute;
+            &eacute;motionnelle avec les autres<span
+                style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(112, 173, 71); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">travailleurs</span>
+    </p>
+</li>
+<li
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; font-size: 10.72px; margin: 0px 0px 0px 20.1px; font-style: inherit; font-weight: inherit;">
+
+    <p
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; font-style: inherit; font-weight: inherit;">
+        <span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Dans
+            un m&ecirc;me temps, dans un cadre organisationnel stable, dans une proximit&eacute;
+            &eacute;motionnelle avec les autres travailleurs,<span
+                style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">mais
+            dans des espaces diff&eacute;rents&nbsp;</span>
+    </p>
+</li>
+<li
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; font-size: 10.72px; margin: 0px 0px 0px 20.1px; font-style: inherit; font-weight: inherit;">
+
+    <p
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; font-style: inherit; font-weight: inherit;">
+        <span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Dans
+            un cadre organisationnel stable, en proximit&eacute; &eacute;motionnelle avec les autres
+            travailleurs mais dans des espaces diff&eacute;rents<span
+                style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">et
+            des temps tr&egrave;s variables</span>
+    </p>
+</li>
+<li
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; font-size: 10.72px; margin: 0px 0px 0px 20.1px; font-style: inherit; font-weight: inherit;">
+
+    <p
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; font-style: inherit; font-weight: inherit;">
+        <span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">A
+            diff&eacute;rents endroits, &agrave; diff&eacute;rents moments, dans diff&eacute;rents cadres
+            organisationnels, sans proximit&eacute; &eacute;motionnelle avec les<span
+                style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(255, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">autres
+            travailleurs</span>
+    </p>
+</li>
+<li
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; font-size: 10.72px; margin: 0px 0px 0px 20.1px; font-style: inherit; font-weight: inherit;">
+
+    <p
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; font-style: inherit; font-weight: inherit;">
+        <br>
+    </p>
+</li>
+</ol>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<br>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Tentatives
+        :</span><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">&nbsp;</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Tentatives
+        infinies</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Passage
+        &agrave; la suite si correct</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<br>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Feedback
+        :&nbsp;</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Si
+        incorrect : Eh non ! Repensez bien aux points communs entre ces situations.</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<br>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Si
+        correct : Tout &agrave; fait !</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Tr&egrave;s
+        majoritairement dans l&#39;histoire, les personnes travaillent selon une unit&eacute; spatiale,
+        temporelle, organisationnelle, et<span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">&eacute;motionnelle
+        : le travail&nbsp;s&#39;effectue dans un&nbsp;m&ecirc;me espace-temps (dans l&rsquo;ici et le
+        maintenant), dans un cadre organisationnel<span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">stable
+        (une seule organisation, des r&egrave;gles bien &eacute;tablies, un responsable avec qui il n&#39;y a
+        pas de coupures), et dans&nbsp;un &eacute;tat d&#39;esprit<span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">globalement
+        partag&eacute;&nbsp;avec les autres travailleurs.</span></span>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<br>
+</p>
+
+<p
+style='-webkit-font-smoothing: antialiased; box-sizing: border-box; margin: 0px; color: rgb(255, 255, 255); font-family: "Segoe UI", system-ui, "Apple Color Emoji", "Segoe UI Emoji", sans-serif; font-size: 14px; font-style: inherit; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: inherit; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px;  text-decoration-style: initial; text-decoration-color: initial;'>
+<span
+    style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 12px; font-style: inherit; font-weight: inherit; margin: 0px;"><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">Vous
+        remarquez aussi que malgr&eacute; cette quadruple unit&eacute;, le travail a pris des formes tr&egrave;s
+        diff&eacute;rentes dans l&#39;histoire. Continuez<span
+            style="-webkit-font-smoothing: antialiased; box-sizing: border-box;">&nbsp;</span></span><span
+        style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: rgb(0, 0, 0); font-size: 13.4px; font-style: inherit; font-weight: inherit; margin: 0px; text-decoration-line: none;">votre
+        exploration en d&eacute;couvrant les 2 principaux facteurs d&#39;&eacute;volution du
+        travail.</span></span>
+</p>
+
+`;
